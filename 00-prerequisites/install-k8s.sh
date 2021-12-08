346cc53feb4dd64ee12c8311b0b02116b81c61ec
chadmcrowell <chadmcrowell@gmail.com>
2021-12-07 19:45:35 -0600

    changed azureuser to $HOME

--- 00-prerequisites/install-k8s.sh
+++ 00-prerequisites/install-k8s.sh
@@ -68,9 +68,9 @@ sudo apt-get install -y kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 ku
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # download weavenet config
-sudo wget -O /home/azureuser/weavenet.yml "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+sudo wget -O ${HOME}/weavenet.yml "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 # download init script for control plane node
-sudo wget -O /home/azureuser/initialize-cluster.sh https://raw.githubusercontent.com/chadmcrowell/cka/main/00-prerequisites/initialize-cluster.sh
+sudo wget -O ${HOME}/initialize-cluster.sh https://raw.githubusercontent.com/chadmcrowell/cka/main/00-prerequisites/initialize-cluster.sh
 
-sudo chmod +x /home/azureuser/initialize-cluster.sh
+sudo chmod +x ${HOME}/initialize-cluster.sh
