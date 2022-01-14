f2fb6fc1b5461b1e772af619cf47b48a07e06700
chadmcrowell <chadmcrowell@gmail.com>
2022-01-14 10:34:22 -0600

    added install katacoda

---
+++ 00-prerequisites/install-k8s-katacoda.sh
@@ -0,0 +1,36 @@
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "step1"
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "kubeadm install"
+sudo apt update -y
+sudo apt -y install vim git curl wget kubelet=1.22.0-00 kubeadm=1.22.0-00 kubectl=1.22.0-00
+echo "memory swapoff"
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
+sudo modprobe overlay
+sudo modprobe br_netfilter
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sysctl --system
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sysctl --system
+sudo apt update
+sudo systemctl enable kubelet
+sudo kubeadm config images pull --cri-socket /run/containerd/containerd.sock --kubernetes-version v1.22.0
+sudo kubeadm init   --pod-network-cidr=192.168.0.0/16   --upload-certs --kubernetes-version=v1.22.0  --control-plane-endpoint=host01 --ignore-preflight-errors=Mem  --cri-socket /run/containerd/containerd.sock
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
+kubectl taint node host01 node-role.kubernetes.io/master:NoSchedule-
+wget https://get.helm.sh/helm-v3.7.2-linux-amd64.tar.gz
+tar -xvf helm-v3.7.2-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/
